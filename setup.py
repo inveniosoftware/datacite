dfb4f384a287b7505b7110ce87cf48ea366ca3e3
Steven Loria <sloria1@gmail.com>
2018-06-08 16:09:07 -0400

    travis: test against python 3.6

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
