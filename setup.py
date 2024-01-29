ed99e1bba370fc1256f3b3a0c56bb9d7ecadb3ea
Tom Morrell <tmorrell@caltech.edu>
2024-01-29 13:31:48 -0800

    setup: migrate steup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -3,80 +3,15 @@
 # This file is part of DataCite.
 #
 # Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2024, California Institute of Technology.
 #
 # DataCite is free software; you can redistribute it and/or modify it
 # under the terms of the Revised BSD License; see LICENSE file for
 # more details.
 
-"""Python API wrapper for the DataCite Metadata Store API."""
+"""Python API wrapper for the DataCite Metadata Store and REST APIs."""
 
-import os
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
-readme = open('README.rst').read()
-history = open('CHANGES.rst').read()
-
-tests_require = [
-    'responses>=0.10.6',
-    'mock>=1.3.0',
-    'pytest-invenio>=1.4.0',
-]
-
-extras_require = {
-    'docs': [
-        'Sphinx>=4.5.0',
-    ],
-    'tests': tests_require,
-}
-
-extras_require['all'] = []
-for reqs in extras_require.values():
-    extras_require['all'].extend(reqs)
-
-setup_requires = [
-    'pytest-runner>=2.6.2',
-]
-
-install_requires = [
-    'jsonschema>=3.0.0',
-    'lxml>=4.5.2',
-    'requests>=2.12.2',
-    'idutils>=1.0.0'
-]
-
-packages = find_packages()
-
-# Get the version string. Cannot be done with import!
-g = {}
-with open(os.path.join('datacite', 'version.py'), 'rt') as fp:
-    exec(fp.read(), g)
-    version = g['__version__']
-
-setup(
-    name='datacite',
-    license='BSD',
-    version=version,
-    description=__doc__,
-    long_description=readme + '\n\n' + history,
-    author='Invenio Collaboration',
-    author_email='info@inveniosoftware.org',
-    url='https://github.com/inveniosoftware/datacite',
-    include_package_data=True,
-    packages=packages,
-    zip_safe=False,
-    extras_require=extras_require,
-    install_requires=install_requires,
-    setup_requires=setup_requires,
-    tests_require=tests_require,
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Topic :: Utilities',
-    ],
-)
+setup()
