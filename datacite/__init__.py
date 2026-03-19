5506d1347a070952d2c2b96c213f44c5fa46d0dd
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-03-19 21:50:22 +0100

    release: v1.4.0

--- datacite/__init__.py
+++ datacite/__init__.py
@@ -3,6 +3,7 @@
 # This file is part of DataCite.
 #
 # Copyright (C) 2015, 2016 CERN.
+# Copyright (C) 2026 Graz University of Technology.
 #
 # DataCite is free software; you can redistribute it and/or modify it
 # under the terms of the Revised BSD License; see LICENSE file for
@@ -14,6 +15,6 @@
 from .client import DataCiteMDSClient
 from .rest_client import DataCiteRESTClient
 
-__version__ = "1.3.1"
+__version__ = "1.4.0"
 
 __all__ = ("DataCiteMDSClient", "DataCiteRESTClient", "__version__")
